26c6bbb861a302b6e81e62622828854bf0eb6f63
Josh Wilson <jwils@seas.upenn.edu>
2014-01-19 12:35:31 -0800

    Return fully qualified urls.

--- lib/page.rb
+++ lib/page.rb
@@ -1,37 +1,61 @@
 require 'nokogiri'
-
+require 'uri'
 
 
 class Page
   def initialize(url, raw_content)
-    @url = url
+    @url = URI(url)
     @content = Nokogiri.HTML(raw_content)
+  end
+
+  def full_url_path(url)
+    case url
+
+    when /^\/\/.*$/ #use same scheme
+      @url.scheme + ":" + url
+    when /^\/[^\/].*$/
+      @url.scheme + "://" + @url.host + url
+    when /^https?:\/\/.*$/
+      url
+    when /^.*:.*/
+      puts "unknown protocol. Ignoring uri:" + url
+      nil
+    when /^#.*/
+      puts "Ignoring # uri:" + url
+      nil
+    else
+      if @url.to_s.end_with?('/')
+        @url.to_s + url
+      else
+        @url.to_s + "/" + url
+      end
+    end
   end
 
   def links
     if @links.nil?
-      @links = @content.xpath('//a/@href').map {|link| link.to_s}
+      @links = @content.xpath('//a/@href').map {|link| full_url_path(link.to_s) }.compact
     end
     @links
   end
 
   def image_links
     if @image_links.nil?
-      @image_links = @content.xpath('//img/@src').map {|link| link.to_s}
+      @image_links = @content.xpath('//img/@src').map {|link| full_url_path(link.to_s)}.compact
     end
     @image_links
   end
 
   def javascript_links
     if @javascript_links.nil?
-      @javascript_links = @content.xpath('//script/@src').map {|link| link.to_s}
+      @javascript_links = @content.xpath('//script/@src').map {|link| full_url_path(link.to_s)}.compact
     end
     @javascript_links
   end
 
   def css_links
     if @css_links.nil?
-      @css_links = @content.xpath('//link/@href').map {|link| link.to_s}
+      @css_links = @content.xpath('//link/@href').map {|link| full_url_path(link.to_s)}.compact
     end
     @css_links
   end
--- spec/page_spec.rb
+++ spec/page_spec.rb
@@ -5,18 +5,18 @@ example_page = File.open('example_page.html', 'rb') { |file| file.read }
 
 describe Page do 
   describe '#links' do
-    it 'returns all links on a page' do
+    it 'returns all links on a page that are http or https' do
       links = Page.new('https://www.joingrouper.com/home', example_page).links
-      expect(links).to include('/reserve')
-      expect(links).to include('/log_out')
-      expect(links.size).to eq(20)
+      expect(links).to include('https://www.joingrouper.com/reserve')
+      expect(links).to include('https://www.joingrouper.com/log_out')
+      expect(links.size).to eq(17)
     end
   end
 
   describe '#image_links' do
     it 'returns the src of all images on a page' do
       links = Page.new('https://www.joingrouper.com/home', example_page).image_links
-      expect(links).to include('/assets/logos/gray-text-logo-13d668a650e4218cf2941e282fda71d7.png')
+      expect(links).to include('https://www.joingrouper.com/assets/logos/gray-text-logo-13d668a650e4218cf2941e282fda71d7.png')
       expect(links.size).to eq(9)
     end
   end
@@ -24,7 +24,7 @@ describe Page do
   describe '#javascript_links' do
     it 'returns the src of all script tags' do
       links = Page.new('https://www.joingrouper.com/home', example_page).javascript_links
-      expect(links).to include('/assets/application-08a1ec4e9dd3cd46ce88383742eb3a75.js')
+      expect(links).to include('https://www.joingrouper.com/assets/application-08a1ec4e9dd3cd46ce88383742eb3a75.js')
       expect(links.size).to eq(6)      
     end
   end
@@ -32,10 +32,72 @@ describe Page do
   describe '#css_links' do
     it 'returns the src of all link tags' do
       links = Page.new('https://www.joingrouper.com/home', example_page).css_links
-      puts links
-      expect(links).to include('/assets/sane-3d3f2ff1544088b125229d1b8ee63f79.css')
+      expect(links).to include('https://www.joingrouper.com/assets/sane-3d3f2ff1544088b125229d1b8ee63f79.css')
       expect(links.size).to eq(1)      
     end
+  end
+
+  describe '#full_url_path' do
+    it "does not change fully qualified urls" do
+      urls = ["https://www.google.com/abadfadsf", "http://www.reddit.com",
+              "https://news.ycombinator.com"]
+
+      page = Page.new('https://www.joingrouper.com/home', example_page)
+      urls.each do |url|
+        expect(page.full_url_path(url)).to eq(url)
+      end
+    end
+
+    it "adds the domain to urls that begin with /" do
+      urls = ["/home", "/some/deep/page.html",
+              "/another/page?with=params"]
+
+      page = Page.new('https://www.joingrouper.com/home', example_page)
+      urls.each do |url|
+        expect(page.full_url_path(url)).to eq("https://www.joingrouper.com" + url)
+      end
+    end
+
+    it "adds the protocol to urls that begin with //" do
+      urls = ["//google.com", "//facebook.com",
+              "//bing.com/with/path"]
+
+      httpsPage = Page.new('https://www.joingrouper.com/home', example_page)
+      urls.each do |url|
+        expect(httpsPage.full_url_path(url)).to eq("https:" + url)
+      end
+
+      httpPage = Page.new('http://www.joingrouper.com/home', example_page)
+      urls.each do |url|
+        expect(httpPage.full_url_path(url)).to eq("http:" + url)
+      end
+    end
+
+
+    it "adds the existing urlpath to urls not starting with /" do
+      urls = ["home", "some/deep/page.html",
+              "another/page?with=params"]
+
+      page = Page.new('https://www.joingrouper.com/home', example_page)
+      urls.each do |url|
+        expect(page.full_url_path(url)).to eq("https://www.joingrouper.com/home/" + url)
+      end
+
+      page2 = Page.new('https://www.joingrouper.com/home/', example_page)
+      urls.each do |url|
+        expect(page.full_url_path(url)).to eq("https://www.joingrouper.com/home/" + url)
+      end
+    end
+
+    it "ignores unknown protocols and # urls" do
+      urls = ["#", "#someExtraInfo",
+              "mailto:", "mailto:anyone@anywhere.com"]
+
+      page = Page.new('https://www.joingrouper.com/home', example_page)
+      urls.each do |url|
+        expect(page.full_url_path(url)).to eq(nil)
+      end
+    end  
   end
 end
 
